be79b3c1b9dc9ce66c455964488400799643746f
sierra.marco0490@outlook.com <sierra.marco0490@outlook.com>
2023-10-12 13:25:10 -0400

    segundo commit

---
+++ src/components/ActionAgente/AccionAgente.jsx
@@ -0,0 +1,8 @@
+
+
+    <div className="hidden md:block w-full py-2 bg-primary text-#0A6BE2">
+        <div>
+        <p className="flex justify-center">
+            </p>
+            
+            </div></div>
---
+++ src/pages/AfterCotizador/AfterCotizador.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AfterCotizador() {
+  return (
+    <div>AfterCotizador</div>
+  )
+}
+
+export default AfterCotizador
