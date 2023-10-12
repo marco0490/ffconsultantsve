23986f85c2d9899b22f21ffa1b6db9d4d37e55e9
sierra.marco0490@outlook.com <sierra.marco0490@outlook.com>
2023-10-12 16:03:30 -0400

    4 commit

--- src/components/ActionAgente/AccionAgente.jsx
+++ src/components/ActionAgente/AccionAgente.jsx
@@ -1,8 +1,20 @@
 
 
-    <div className="hidden md:block w-full py-2 bg-primary text-#0A6BE2">
+
+function AcionAgente() {
+    return (
+
+    <div className="hidden md:block w-full py-2 bg-primary text-blue">
         <div>
         <p className="flex justify-center">
-            </p>
+        Trabajemos juntos.
+        <Link to="/cotizador" className="flex ms-2 font-bold hover:text-purple">
+          Unete Aqui{' '}
+          <span className='inline-block'>
+            <BiRightArrowAlt className="text-2xl" />
+          </span>
+        </Link>
+      </p>
             
             </div></div>
+            ); }
