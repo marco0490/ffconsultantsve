734ec4c3246209f356f46a1495462e863ca656be
sierra.marco0490@outlook.com <sierra.marco0490@outlook.com>
2023-10-16 13:43:44 -0400

    1 commit rama dest

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,23 @@
-
+import { useEffect } from 'react'
+import ActionSection from '../../components/ActionSection/ActionSection'
+import CompaniesPlan from '../../components/CompaniesPlan/CompaniesPlan'
+import Howto from '../../components/Howto/Howto'
+import AccionAgente from '../../components/AccionAgente/AccionAgente'
+import Testimonials from '../../components/Testimonials/Testimonials'
 
 function Home() {
+  useEffect(() => {
+    window.scroll(0, 0)
+  }, [])
   return (
-    <div>
-      <h1>HOME</h1>
-      
-    </div>
+    <>
+      <ActionSection />
+      <Howto />
+      <CompaniesPlan />
+      <AccionAgente />
+      <Testimonials />
+    </>
   )
 }
 
-export default Home
+export default AccionAgente
