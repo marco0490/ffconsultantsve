c7807bef32f5bb27e7d5b429a2b6bf21104aa0e6
sierra.marco0490@outlook.com <sierra.marco0490@outlook.com>
2023-10-17 17:21:44 -0400

    marco commit

--- src/components/AccionAgente/AccionAgente.jsx
+++ src/components/AccionAgente/AccionAgente.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 function AccionAgente() {
   return (
 
-    <div className="hidden md:block w-full py-2 bg-primary text-blue">
+    <div className="hidden md:block w-full py-2 bg-primary text-blue max-w-[1900px] mx-auto">
       <div>
         <p className="flex justify-center">
           Trabajemos juntos.
