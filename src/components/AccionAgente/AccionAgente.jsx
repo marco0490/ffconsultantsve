c26806fc75db680a96371f1ce27898393a7f68e8
sierra.marco0490@outlook.com <sierra.marco0490@outlook.com>
2023-10-25 13:41:50 -0400

    ultimo

--- src/components/AccionAgente/AccionAgente.jsx
+++ src/components/AccionAgente/AccionAgente.jsx
@@ -9,7 +9,7 @@ function AccionAgente() {
     <div className="hidden md:block w-full py-2 bg-primary text-blue max-w-[1900px] mx-auto">
       <div>
         <p className="flex justify-center">
-          Trabajemos juntos.
+        Aun click de vivir la esperiencia brutal de un servicio de salud digital de primera.
           <Link to="/cotizador" className="flex ms-2 font-bold hover:text-purple">
             Unete Aqui{' '}
             <span className='inline-block'>
