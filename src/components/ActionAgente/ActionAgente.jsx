c761d2691e2043b4b69613c434792071be721b81
sierra.marco0490@outlook.com <sierra.marco0490@outlook.com>
2023-10-30 17:29:36 -0400

    action action

--- src/components/ActionAgente/ActionAgente.jsx
+++ src/components/ActionAgente/ActionAgente.jsx
@@ -1,4 +1,3 @@
-import { BiRightArrowAlt } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 
 
@@ -6,7 +5,7 @@ function ActionAgente() {
   return (
 
     <div className="w-[1440px] h-[360px] px-[304px] pt-[54px] pb-[53px] bg-blue-800 justify-center items-center inline-flex">
-      <p className="flex justify-center">
+      <p className="flex justify-center space-evently">
         Trabajemos juntos.
         <Link to="/cotizador" className="flex ms-2 font-bold hover:text-purple">
           Unete Aqui{' '}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import ActionSection from '../../components/ActionSection/ActionSection'
 import CompaniesPlan from '../../components/CompaniesPlan/CompaniesPlan'
 import Howto from '../../components/Howto/Howto'
-import ActionAgente from '../../components/AcionAgente/AcionAgente'
+import ActionAgente from '../../components/ActionAgente/ActionAgente'
 import Testimonials from '../../components/Testimonials/Testimonials'
 
 function Home() {
