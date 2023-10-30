b92f0085786ac081d6cf414497aefb3c4947bd63
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-10-30 22:24:57 +0100

    Refactor: restructure Companies Plan section

--- src/components/CompaniesPlan/CompaniesPlan.jsx
+++ src/components/CompaniesPlan/CompaniesPlan.jsx
@@ -1,32 +1,34 @@
 import { Link } from 'react-router-dom'
 import MercantilLogo from '../../assets/images/MercantilLogo.svg'
 import QualitasLogo from '../../assets/images/QualitasLogo.svg'
-import {BiRightArrowAlt} from 'react-icons/bi'
+import { BiRightArrowAlt } from 'react-icons/bi'
 
 function CompaniesPlan() {
   return (
-    <div className="grid md:px-32 grid-cols-1 sm:grid-cols-2 gap-8 text-center mx-2 md:pt-28 max-w-[1900px]">
-      <card>
-        <img
-          src={MercantilLogo}
-          alt="Logo Mercantil"
-          className="mx-auto my-12"
-        />
-        <p className="max-w-[600px] mx-auto">
-          Mercantil Seguros cuenta con una trayectoria de 30 años en el mercado
-          asegurador, ha logrado ganarse la confianza de los venezolanos.
-        <Link to="/planes-mercantil" className='flex justify-end mt-6 text-primary font-bold hover:text-purple'>Ver planes y servicios <BiRightArrowAlt className='text-2xl'/></Link>
-        </p>
-      </card>
-      <card>
-        <img src={QualitasLogo} alt="Logo Qualitas" className="mx-auto my-12" />
-        <p className="max-w-[600px] mx-auto">
-          Empresa de referencia en el mercado asegurador venezolano, basados en
-          la excelencia de sus productos y servicios, comprometidos con la
-          tranquilidad y el bienestar de sus asegurados.
-          <Link to="/planes-qualitas" className='flex justify-end mt-6 text-primary font-bold hover:text-purple'>Ver planes y servicios <BiRightArrowAlt className='text-2xl'/></Link>
-        </p>
-      </card>
+    <div className="wrapper region">
+      <div className="plans auto-grid">
+        <card className="stack max-w-[37.5rem]">
+          <img
+            src={MercantilLogo}
+            alt="Logo Mercantil"
+            className="mx-auto my-12"
+          />
+          <p className="mx-auto mb-auto">
+            Mercantil Seguros cuenta con una trayectoria de 30 años en el mercado
+            asegurador, ha logrado ganarse la confianza de los venezolanos.
+          </p>
+          <Link to="/planes-mercantil" className='flex justify-end mt-6 text-primary font-bold hover:text-purple'>Ver planes y servicios <BiRightArrowAlt className='text-2xl' /></Link>
+        </card>
+        <card className="stack max-w-[37.5rem]">
+          <img src={QualitasLogo} alt="Logo Qualitas" className="mx-auto my-12" />
+          <p className="mx-auto mb-auto">
+            Empresa de referencia en el mercado asegurador venezolano, basados en
+            la excelencia de sus productos y servicios, comprometidos con la
+            tranquilidad y el bienestar de sus asegurados.
+          </p>
+          <Link to="/planes-qualitas" className='flex justify-end mt-6 text-primary font-bold hover:text-purple'>Ver planes y servicios <BiRightArrowAlt className='text-2xl' /></Link>
+        </card>
+      </div>
     </div>
   )
 }
