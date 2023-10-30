3a040d0147684190e166b20638cfe4411b655ba8
sierra.marco0490@outlook.com <sierra.marco0490@outlook.com>
2023-10-30 17:11:49 -0400

    actionagente

---
+++ src/components/ActionAgente/ActionAgente.jsx
@@ -0,0 +1,23 @@
+import { BiRightArrowAlt } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+
+
+function ActionAgente() {
+  return (
+
+    <div className="w-[1440px] h-[360px] px-[304px] pt-[54px] pb-[53px] bg-blue-800 justify-center items-center inline-flex">
+      <p className="flex justify-center">
+        Trabajemos juntos.
+        <Link to="/cotizador" className="flex ms-2 font-bold hover:text-purple">
+          Unete Aqui{' '}
+          <span className='inline-block'>
+            <BiRightArrowAlt className="text-2xl" />
+          </span>
+        </Link>
+      </p>
+
+
+    </div>
+  );
+}
+export default ActionAgente
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import ActionSection from '../../components/ActionSection/ActionSection'
 import CompaniesPlan from '../../components/CompaniesPlan/CompaniesPlan'
 import Howto from '../../components/Howto/Howto'
-import AccionAgente from '../../components/AccionAgente/AccionAgente'
+import ActionAgente from '../../components/AcionAgente/AcionAgente'
 import Testimonials from '../../components/Testimonials/Testimonials'
 
 function Home() {
@@ -14,7 +14,7 @@ function Home() {
       <ActionSection />
       <Howto />
       <CompaniesPlan />
-      <AccionAgente />
+      <ActionAgente />
       <Testimonials />
     </>
   )
