c1b52a0b6c99cb6477b760a262e6b5fa5c9f5fdb
Carlos Daniel Dominguez <carlosddominguezc@gmail.com>
2023-11-07 21:25:32 -0600

    fixed family background from we

--- src/pages/We/We.jsx
+++ src/pages/We/We.jsx
@@ -10,33 +10,28 @@ function We() {
   }, [])
   return (
     <div className="md:mt-12 mx-2 text-center">
-      <figure className="flex justify-center mx-4">
-        <img
-          src={Family}
-          alt="family"
-          className="absolute items-center max-w-full mx-auto bg-gradient-to-r from-gray-500 to-gray-500"
-        />
-        <div className="absolute max-w-[1100px] mt-8 md:mt-48">
-          <p className="flex justify-center items-center text-white font-normal text-xl md:text-5xl [text-shadow:_0_5px_0_rgb(0_0_0_/_70%)]">
+      <div className={`w-[400px] h-[280px] mx-auto md:w-full md:h-screen flex items-center justify-center bg-['${Family}'] bg-cover bg-no-repeat bg-cente`}>
+        <div className="w-1/2 text-center">
+          <p className="flex justify-center items-center text-white font-normal text-xs md:text-3xl [text-shadow:_0_2px_0_rgb(0_0_0_/_70%)]">
             En FFC mejoramos la vida de nuestros clientes y su familia
           </p>
-          <p className="flex justify-center items-center text-white font-bold text-md md:text-4xl [text-shadow:_0_5px_0_rgb(0_0_0_/_70%)]">
+          <h1 className="flex justify-center items-center text-white font-bold text-sm md:text-4xl [text-shadow:_0_2px_0_rgb(0_0_0_/_70%)]">
             asegurando su bienestar y protección
-          </p>
-          <p className="flex justify-center items-center text-white font-normal text-sm md:text-3xl [text-shadow:_0_5px_0_rgb(0_0_0_/_70%)]">
+          </h1>
+          <p className="flex justify-center items-center text-white font-normal text-xs md:text-3xl [text-shadow:_0_2px_0_rgb(0_0_0_/_70%)]">
             para el futuro
           </p>
           <div className="flex justify-center pt-6 md:pt-12">
-            <Link to="/cotizador">
-              <button className="bg-primary text-white text-center text-xs w-36 md:text-lg md:w-48 md:py-4 py-2 font-semibold border border-white">
-                Únete a nosotros
-              </button>
+          <Link to="/cotizador">
+            <button className="bg-[#3140B9] text-white text-center text-xs w-36 md:text-lg md:w-48 md:py-4 py-2">
+              Únete a nosotros
+            </button>
             </Link>
           </div>
         </div>
-      </figure>
-      <div className="mt-[calc(85%-80px)] md:mt-[550px] lg:mt-[650px] flex flex-col">
-        <h1 className="text-primary flex justify-center text-xl md:text-5xl font-bold">
+      </div>
+      <div className="mt-10 md:mt-20 flex flex-col">
+        <h1 className="text-primary flex justify-center text-base md:text-5xl font-light">
           ¿Quiénes somos?
         </h1>
         <p className="max-w-[1100px] text-center mx-auto text-lg md:text-3xl mt-2 md:mt-10 font-normal">
@@ -63,12 +58,12 @@ function We() {
         <div className="bg-primary w-full h-48 md:h-[450px] -top-44 md:top-[-250px] z-[-1] relative"></div>
       </div>
 
-        <p className="text-primary text-2xl md:text-7xl font-bold flex justify-center -mt-24">
-          Preguntas frecuentes
-        </p>
-        <p className="max-w-[1100px] text-center mx-auto text-base md:text-4xl mt-8 font-semibold py-5">
-          Si tienes alguna duda, chequea nuestras preguntas frecuentes aquí.
-        </p>
+      <p className="text-primary text-2xl md:text-7xl font-bold flex justify-center -mt-24">
+        Preguntas frecuentes
+      </p>
+      <p className="max-w-[1100px] text-center mx-auto text-base md:text-4xl mt-8 font-semibold py-5">
+        Si tienes alguna duda, chequea nuestras preguntas frecuentes aquí.
+      </p>
 
       <div className="w-full flex mx-auto">
         <div className="py-2 mx-auto max-w-7xl">
