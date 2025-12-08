c5ae88904a778adfde004aae1cf65d893d2dd0fd
soyandresjr1703 <andresevp1703@gmail.com>
2025-12-08 17:58:00 -0400

    Se agrego PlansOceanica

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -82,6 +82,19 @@ function Footer() {
                     </Link>
                   )}
                 </Menu.Item>
+                 <Menu.Item>
+                  {({ active }) => (
+                    <Link
+                      to="/planes-oceanica"
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm',
+                      )}
+                    >
+                      Oceanica
+                    </Link>
+                  )}
+                </Menu.Item>
                 {/*
                 <Menu.Item>
                   {({ active }) => (
